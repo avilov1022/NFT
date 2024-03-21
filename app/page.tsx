b12f7831b0e445ab4b80b560fcf89adaa53b1583
avilov1022 <118724722+avilov1022@users.noreply.github.com>
2024-03-21 10:58:54 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import styles from "./page.css";
+import HeroSection from "@/widgets/HeroSection/HeroSection";
+import TrendingCollection from "@/widgets/TrendingCollection/TrendingCollection";
+import TopCreators from "@/widgets/TopCreators/TopCreators";
+import BrowseCategories from "@/widgets/BrowseCategories/BrowseCategories";
+import DiscoverMore from "@/widgets/DiscoverMore/DiscoverMore";
+import MagicMashrooms from "@/widgets/MagicMashrooms/MagicMashrooms";
+import HowItWorks from "@/widgets/HowItWorks/HowItWorks";
+import Subscribe from "@/widgets/Subscribe/Subscribe";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <HeroSection/>
+      <TrendingCollection/>
+      <TopCreators/>
+      <BrowseCategories/>
+      <DiscoverMore/>
+      <MagicMashrooms/>
+      <HowItWorks/>
+      <Subscribe/>
+    </main>
+  );
+}
